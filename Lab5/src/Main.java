56e6ac43d9034310142b76cd5cc42e8cf8062eb7
Ariana Hategan <arianahategan@yahoo.com>
2020-11-18 11:41:21 +0200

    Added grammar class

---
+++ Lab5/src/Main.java
@@ -0,0 +1,9 @@
+import grammar.Grammar;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Grammar grammar = new Grammar("grammar.in");
+        System.out.println(grammar);
+    }
+}
